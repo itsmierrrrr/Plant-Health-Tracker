4b0125a7cbdf6e95cc2c15d20173b34d108889e8
Mihir <mihir.s.sawant17@gmail.com>
2026-06-13 21:20:15 +0530

    auth

---
+++ src/types/auth.ts
@@ -0,0 +1,21 @@
+export type AuthUser = {
+  id: string;
+  name: string;
+  email: string;
+  createdAt?: string;
+  updatedAt?: string;
+};
+
+export type AuthSession = {
+  user: AuthUser;
+  token: string;
+};
+
+export type AuthCredentials = {
+  email: string;
+  password: string;
+};
+
+export type RegisterCredentials = AuthCredentials & {
+  name: string;
+};
