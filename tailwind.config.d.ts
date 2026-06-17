b300f3b994b04c297ff9b54969de5f71367ebd0c
Mihir <mihir.s.sawant17@gmail.com>
2026-06-17 21:49:50 +0530

    tailwind conf ts

---
+++ tailwind.config.d.ts
@@ -0,0 +1,70 @@
+declare const _default: {
+    content: string[];
+    theme: {
+        extend: {
+            colors: {
+                brand: {
+                    50: string;
+                    100: string;
+                    200: string;
+                    300: string;
+                    400: string;
+                    500: string;
+                    600: string;
+                    700: string;
+                    800: string;
+                    900: string;
+                };
+                leaf: {
+                    50: string;
+                    100: string;
+                    200: string;
+                    300: string;
+                    400: string;
+                    500: string;
+                    600: string;
+                    700: string;
+                    800: string;
+                    900: string;
+                };
+                cream: string;
+            };
+            boxShadow: {
+                soft: string;
+                lift: string;
+            };
+            backgroundImage: {
+                'hero-glow': string;
+                mesh: string;
+            };
+            keyframes: {
+                float: {
+                    '0%, 100%': {
+                        transform: string;
+                    };
+                    '50%': {
+                        transform: string;
+                    };
+                };
+                shimmer: {
+                    '0%': {
+                        backgroundPosition: string;
+                    };
+                    '100%': {
+                        backgroundPosition: string;
+                    };
+                };
+            };
+            animation: {
+                float: string;
+                shimmer: string;
+            };
+            fontFamily: {
+                sans: [string, string, string];
+                display: [string, string];
+            };
+        };
+    };
+    plugins: any[];
+};
+export default _default;
